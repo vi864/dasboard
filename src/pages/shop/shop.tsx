235969f3cf32bf3bc5934243a4f55443c0a6efe0
vi864 <vishaladhikati703@gmail.com>
2025-02-16 21:11:55 +0530

    Updated feature

--- src/pages/shop/shop.tsx
+++ src/pages/shop/shop.tsx
@@ -1,12 +1,15 @@
-import React from "react";
-import headphone from "../../images/shop/wireless.jpg"
-import { FaShoppingCart } from "react-icons/fa";
+import React from 'react';
+import headphone from '../../images/shop/wireless.jpg';
+import watch from '../../images/shop/samrtwatch.jpeg';
+import mouse from'../../images/shop/mouse.jpeg';
+import keyboard from'../../images/shop/keyboard.jpg';
+import { FaShoppingCart } from 'react-icons/fa';
 
 const products = [
-  { id: 1, name: "Wireless Headphones", price: "$99", image: headphone },
-  { id: 2, name: "Smart Watch", price: "$149", image: headphone },
-  { id: 3, name: "Gaming Mouse", price: "$49", image: headphone },
-  { id: 4, name: "Mechanical Keyboard", price: "$129", image: headphone },
+  { id: 1, name: 'Wireless Headphones', price: '$99', image: headphone },
+  { id: 2, name: 'Smart Watch', price: '$149', image: watch},
+  { id: 3, name: 'Gaming Mouse', price: '$49', image: mouse },
+  { id: 4, name: 'Mechanical Keyboard', price: '$129', image: keyboard },
 ];
 
 const Shop = () => {
@@ -16,11 +19,11 @@ const Shop = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products.map((product) => (
           <div key={product.id} className="bg-white shadow-lg rounded-lg p-4">
-           <img 
-  src={product.image} 
-  alt={product.name} 
-  className="w-full h-40 object-contain rounded-md"
-/>
+            <img
+              src={product.image}
+              alt={product.name}
+              className="w-full h-40 object-contain rounded-md"
+            />
 
             <h3 className="text-lg font-semibold mt-2">{product.name}</h3>
             <p className="text-gray-600">{product.price}</p>
--- src/pages/stats/Stats.tsx
+++ src/pages/stats/Stats.tsx
@@ -8,7 +8,7 @@ const Stats = () => {
                 <Breadcrumb pageName="Stats" />
             </div>
             <div className="max-w-xl h-[70vh] border bg-white p-3 dark:bg-gray-800 dark:border-0 rounded-lg">
-                {/* <ActivityChart /> */}
+                <ActivityChart />
             </div>
         </div>
     )
